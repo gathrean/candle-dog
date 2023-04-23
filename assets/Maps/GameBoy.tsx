8848e302fdf23a0f5c653bb9f436e8c708203abc
Gathrean Dela Cruz <gathrean@icloud.com>
2023-04-22 21:31:56 -0700

    Added map tiles for testing purposes.

---
+++ assets/Maps/GameBoy.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="GameBoy" tilewidth="16" tileheight="16" tilecount="220" columns="11">
+ <image source="GameBoy Tile.png" width="176" height="320"/>
+ <tile id="42" probability="-3"/>
+</tileset>
