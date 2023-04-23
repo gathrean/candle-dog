2350d766832df9f360e211c86440fc2e81059dff
Gathrean Dela Cruz <gathrean@icloud.com>
2023-04-22 18:25:06 -0700

    Added JavaDocs

--- src/main/java/com/candledog/game/Main.java
+++ src/main/java/com/candledog/game/Main.java
@@ -1,5 +1,11 @@
 package com.candledog.game;
 
+/**
+ * Main class.
+ *
+ * @author Gathrean Dela Cruz
+ * @version 1.0
+ */
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
