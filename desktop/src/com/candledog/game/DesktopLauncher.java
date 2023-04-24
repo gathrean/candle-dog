abb69fc56ab5bf9191b008e6cb1e13fca0a7a638
Gathrean Dela Cruz <gathrean@icloud.com>
2023-04-23 18:42:25 -0700

    Added code to set minimum window size 800x800 and maximum to 3000x3000. Also, set Resizable window to false. Set IdleFPS to 0, but I'm not sure yet if that will be helpful

--- desktop/src/com/candledog/game/DesktopLauncher.java
+++ desktop/src/com/candledog/game/DesktopLauncher.java
@@ -7,9 +7,18 @@ import com.candledog.game.Game;
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+
+		config.setWindowedMode(800, 800);
+		config.setIdleFPS(0);
+		config.setResizable(false);
+		config.setWindowSizeLimits(800, 800, 3000, 3000);
+
 		config.setForegroundFPS(60);
 		config.setTitle("Candle Dog");
+
+
 		new Lwjgl3Application(new Game(), config);
 	}
 }
